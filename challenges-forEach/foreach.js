e50b0961a779798745eb9150c816025ac18e8610
wellpinho <wellpinho@outlook.com>
2020-01-10 17:22:50 -0300

    challenge forEach and more...

---
+++ challenges-forEach/foreach.js
@@ -0,0 +1,44 @@
+// Create an array of 5 people names
+
+// Using forEach display all names in console
+
+// Now display the first name
+
+// Now display last name
+
+// Now remove the first name of array
+
+// Remove the last name of array
+
+// Now insert in front of the array your first name
+
+// Insert your last name in last position of array
+
+
+
+/*
+** NOW CREATE AN ARRAY WITH FIVE INTERGER NUMBERS
+*/
+
+// display all in console.log() with forEach
+
+// Insert a number 5.5 in last position
+
+// Remove the numer of third position
+
+// display the sum of the numbers
+
+// what does forEach do?
+
+//hat method do we use to remove in the first position of the array?
+
+// And the remove of the last position?
+
+//And add in first?
+
+// And add ins last?
+
+
+
+
+
